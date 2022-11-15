31b5c6fcf23e3c1691066d33602c43178c056c59
IUT GEII Toulon Table 5 <touloniut@gmail.com>
2022-11-15 15:43:05 +0100

    communication réussi

--- C#/RobotInterfaceYannMasinski/MainWindow.xaml.cs
+++ C#/RobotInterfaceYannMasinski/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ExtendedSerialPort;
+using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,44 @@ namespace RobotInterfaceYannMasinski
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+        ReliableSerialPort serialPort1;
         public MainWindow()
         {
             InitializeComponent();
+            serialPort1= new ReliableSerialPort("COM4", 115200, Parity.None, 8, StopBits.One );
+            serialPort1.Open();
         }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonEnvoyer.Background == Brushes.RoyalBlue){
+                buttonEnvoyer.Background = Brushes.Beige;
+            } else { buttonEnvoyer.Background = Brushes.RoyalBlue; 
+            }
+            SendMessage();
+        }
+
+        private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+        }
+        private void SendMessage()
+        {
+            Reception.Text += "Reçu : " + textBoxEmission.Text + "\n";
+            serialPort1.WriteLine("Reçu : " + textBoxEmission.Text + "\n");
+            textBoxEmission.Text = "";
+        }
+
     }
 }
