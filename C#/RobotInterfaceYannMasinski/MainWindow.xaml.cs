389826b17c01d3f7c1e1a7f68f6cf7a21f370c55
IUT GEII Toulon Table 5 <touloniut@gmail.com>
2022-11-15 16:49:26 +0100

    1.3 Structuration du code à l'aide d'une classe robot

--- C#/RobotInterfaceYannMasinski/MainWindow.xaml.cs
+++ C#/RobotInterfaceYannMasinski/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace RobotInterfaceYannMasinski
 {
@@ -24,11 +25,33 @@ namespace RobotInterfaceYannMasinski
     public partial class MainWindow : Window
     {
         ReliableSerialPort serialPort1;
+        string receivedText;
+        DispatcherTimer timerAffichage;
+
         public MainWindow()
         {
             InitializeComponent();
             serialPort1= new ReliableSerialPort("COM4", 115200, Parity.None, 8, StopBits.One );
+            serialPort1.DataReceived += serialPort1_DataReceived;
             serialPort1.Open();
+            timerAffichage= new DispatcherTimer();
+            timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timerAffichage.Tick += TimerAffichage_Tick; ;
+            timerAffichage.Start();
+        }
+
+        private void TimerAffichage_Tick(object sender, EventArgs e)
+        {
+            if (receivedText != "")
+            {
+                Reception.Text += "Reçu : " + receivedText + "\n";
+                receivedText = "";
+            }
+        }
+
+        public void serialPort1_DataReceived(object sender, DataReceivedArgs e)
+        {
+            receivedText+= Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
         }
 
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -45,6 +68,11 @@ namespace RobotInterfaceYannMasinski
             SendMessage();
         }
 
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            Reception.Text = "";
+        }
+
         private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
@@ -55,7 +83,7 @@ namespace RobotInterfaceYannMasinski
         private void SendMessage()
         {
             Reception.Text += "Reçu : " + textBoxEmission.Text + "\n";
-            serialPort1.WriteLine("Reçu : " + textBoxEmission.Text + "\n");
+            serialPort1.WriteLine(textBoxEmission.Text);
             textBoxEmission.Text = "";
         }
 
