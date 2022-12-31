2df3d4badc50b954f028a0319a9bab175241bec9
yann-masinski <yann.masinski@gmail.com>
2022-12-31 11:50:29 +0100

    Update timer.c

--- Embedded/Robot_Masinski.X/timer.c
+++ Embedded/Robot_Masinski.X/timer.c
@@ -84,7 +84,6 @@ void InitTimer4(float freq) {
 
 
 //Interruption du timer 1
-
 void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void) {
     IFS0bits.T1IF = 0;
     //LED_BLANCHE = !LED_BLANCHE;
